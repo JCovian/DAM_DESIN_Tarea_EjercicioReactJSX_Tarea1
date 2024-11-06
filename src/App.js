e8f6b05c7db56e9312ff8501776b76e075b0028a
Jose Covian <jose@covian.es>
2024-11-06 12:29:38 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Saludo from './Saludo';
+import Despedida from './Despedida';
 
 function App() {
+  const mostrarSaludo = true; // Cambia a false para mostrar el mensaje de despedida
+  const nombreUsuario = 'Jose';
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          {mostrarSaludo ? <Saludo nombre={nombreUsuario} /> : <Despedida nombre={nombreUsuario} />}
+      </div>
       </header>
     </div>
   );
